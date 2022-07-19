84cedc4b1a89ada6dda2a7882e004bcc4d308c6c
Michelle da Rosa Machado <machado.profile@gmail.com>
2022-07-19 12:36:45 -0300

    tentativa para utilizar o map de imagens

--- projeto-pokedex/src/Components/Home.js
+++ projeto-pokedex/src/Components/Home.js
@@ -1,55 +1,71 @@
-import React, {useEffect, useState} from 'react'
-import {useNavigate, useParams} from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
 import * as P from './styled'
 import axios from 'axios'
 
 export default function Home() {
-    const [pokemons, setPokemon] = useState([])
-    const [image, setImage] = useState("")
-    const [contador, setContador] = useState(1)
-    const navigate = useNavigate ()
-    const params = useParams()
-  
-    const detalhes = () => {
-      navigate("/detalhes")
-    }
-  
-    const pokedex = () => {
-      navigate("/pokedex")
-    }
+  const [pokemons, setPokemon] = useState([])
+  const [image, setImage] = useState([])
+  const [contador, setContador] = useState(1)
+  const navigate = useNavigate()
+  const params = useParams()
 
-    useEffect(() => {
-      getPokemons()
-    },[] )
+  const detalhes = () => {
+    navigate("/detalhes")
+  }
 
-  
-    const getPokemons = (id) => {
-      axios.get(`https://pokeapi.co/api/v2/pokemon/35`)
-      .then ((res) => {
+  const pokedex = () => {
+    navigate("/pokedex")
+  }
+
+
+  useEffect(() => {
+    // if (contador < 20) {
+    //   setContador(contador + 1)
+    // } else {
+    //   setContador(contador)
+    // };
+    getPokemons()
+  }, [contador])
+
+
+  const getPokemons = () => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${contador}`)
+      .then((res) => {
+        console.log(contador)
         setImage(res.data.sprites.front_default)
+
       })
       .catch((err) => {
         console.log(err)
 
       })
 
-    }
-         
-    return (
-      <>
-            
-        <button onClick={detalhes}> Detalhes </button>
-        <button onClick={pokedex}> Pokedex </button>
+  }
+
+  return (
+    <>
+
+      <button onClick={detalhes}> Detalhes </button>
+      <button onClick={pokedex}> Pokedex </button>
 
       <P.Container>
         <ion-icon size="large" name="arrow-back-outline"></ion-icon>
       </P.Container>
 
       <P.ContainerCard>Titulo Qualquer</P.ContainerCard>
-      
-          <img src={image} />
 
-    
-      </>
-    )
-  }
+      <img src={image} />
+
+      {/* {
+        image.map((poke) => {
+          return (
+          <img src={poke} />)
+        })
+      } */}
+
+
+
+    </>
+  )
+}

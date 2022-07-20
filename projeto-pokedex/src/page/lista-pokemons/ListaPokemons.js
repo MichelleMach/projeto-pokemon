7ef8b26c90d282f1917b306f62c014be18fe18d3
Michelle da Rosa Machado <machado.profile@gmail.com>
2022-07-20 18:05:44 -0300

    lista-pokemon e menu

--- projeto-pokedex/src/page/lista-pokemons/ListaPokemons.js
+++ projeto-pokedex/src/page/lista-pokemons/ListaPokemons.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import Menu from '../../Components/menu/Menu'
 import * as P from './styled'
 import axios from 'axios'
 
@@ -18,7 +19,7 @@ export default function Home() {
 
 
   useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon`)
+    axios.get(`https://pokeapi.co/api/v2/pokemon?limit-288offset=8`)
       .then((res) => {
         setListaPokemon(res.data.results)
         console.log(res.data.results)
@@ -33,15 +34,15 @@ export default function Home() {
     listaPokemon.forEach((poke) => {
       axios.get(`https://pokeapi.co/api/v2/pokemon/${poke.name}`)
         .then((res) => {
-         pokemonLista.push(res.data)
-         if(pokemonLista.length ===20) {
-          setPokemon(pokemonLista)
-         }
+          pokemonLista.push(res.data)
+          if (pokemonLista.length === 20) {
+            setPokemon(pokemonLista)
+          }
         }).catch((err) => {
           console.log(err.response)
         })
     })
-  
+
   }, [listaPokemon])
 
   console.log(pokemon)
@@ -50,28 +51,34 @@ export default function Home() {
   return (
     <div>
 
+      <Menu />
+
       <button onClick={detalhes}> Detalhes </button>
       <button onClick={pokedex}> Pokedex </button>
 
-      <P.Container>
+      {/* <P.Container>
         <ion-icon size="large" name="arrow-back-outline"></ion-icon>
-      </P.Container>
+      </P.Container> */}
 
       {/* <P.ContainerCard>Titulo Qualquer</P.ContainerCard> */}
 
-    <P.Container>
-      {
-        pokemon.map((poke) => {
-          return (
-            <P.ContainerCard> 
-              <img src={poke.sprites.other.dream_world.front_default} />
-              <button>Adicionar a Pokedex</button>
-              <button>Ver Detalhes</button>
-              {/* {console.log(poke.sprites)} */}
-            </P.ContainerCard>
-          )
-        })
-      }
+      <P.Container>
+
+        {
+          pokemon.map((poke) => {
+            return (
+              <P.ContainerCard>
+                <P.ImagemPoke src={poke.sprites.other.dream_world.front_default} />
+
+                <P.Botoes>
+                  <P.BotaoAdicionar>Adicionar a Pokedex</P.BotaoAdicionar>
+                  <P.BotaoDetalhe>Ver Detalhes</P.BotaoDetalhe>
+                </P.Botoes>
+
+              </P.ContainerCard>
+            )
+          })
+        }
       </P.Container>
 
       {/* {ListarPokemon} */}
--- projeto-pokedex/src/page/lista-pokemons/styled.js
+++ projeto-pokedex/src/page/lista-pokemons/styled.js
@@ -5,20 +5,66 @@ export const Container = styled.section `
     justify-content:space-evenly;
     flex-wrap:wrap;
 
-`
+`;
 
 export const ContainerCard = styled.div `
-    width:250px;
-    height:300px;
+    width:230px;
+    height:350px;
     display:flex;
     flex-direction:column;
-    justify-content:center;
-    margin-right:50px;
-    border: 1px solid;
+    margin:40px 20px;
+    border: 3px solid #D9D9D9;
+    background-color: #D9D9D9;
+`;
 
-    img{
-        width:150px;
-        height:150px;
+export const ImagemPoke = styled.img `
+    display:flex;
+    align-items:center;
+    width:230px;
+    height:250px;
+    /* &:hover {
+    transform: scale(1.2);
+    transition: all 0.5s;
     }
+    &:hover:after {
+    transform: scale(1);
+    transition: 1s;
+    } */
+
+`;
+
+export const BotaoAdicionar = styled.button `
+    background-color: #EE4035;
+    border-radius: 30px;
+    padding:8px;
+    width: 220px;
+    color:white;
+    border:none;
+    font-weight: bold;
+    &:hover {
+        cursor:pointer
+    }
+`;
+
+export const BotaoDetalhe = styled.button `
+    background-color: #FBF03E;
+    border-radius: 30px;
+    padding:8px;
+    width: 220px;
+    color:#5A6990;
+    border:none;
+    font-weight: bold;
+    &:hover {
+        cursor:pointer
+    }
+`;
+
+export const Botoes = styled.div `
+    display:flex;
+    align-items:center;
+    flex-direction: column;
+    justify-content: space-evenly;
+    background-color: white;
+    height: 100px;
 `
 

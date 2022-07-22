c3f17a4f96deecd953fdb81352f60ce38bcf3fe9
Melissa Fernanda Copetti <melissacopetti@hotmail.com>
2022-07-22 02:11:39 -0300

    globalState feito, botão do card.b mudado temporariamente

--- projeto-pokedex/src/Components/Pokedex/Pokedex.js
+++ projeto-pokedex/src/Components/Pokedex/Pokedex.js
@@ -1,50 +1,50 @@
-import React, { useState} from 'react'
-import {useNavigate} from 'react-router-dom'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 
 
-export default function Pokedex(props) {
-  const navigate = useNavigate ()
-  const [pokedex, setPokedex] = useState({})
- 
-const removerPokemon = (novoPokemon)=>{
-  setPokedex(pokedex.filter(pokemon =>novoPokemon.name !== pokemon.name))
-}
+export default function Pokedex() {
+  const navigate = useNavigate()
 
-const detalhes = () => {
-  navigate("/detalhes")
+  const removerPokemon = (novoPokemon) => {
+    setPokedex(state.pokedex.filter(pokemon => novoPokemon.name !== pokemon.name))
   }
-  
+
+  const detalhes = () => {
+    navigate("/detalhes")
+  }
+
   const home = () => {
     navigate("/")
   }
 
 
-  
-
-  const exibirPokemons = pokedex?.map((pokemon)=>{
-console.log(pokedex)
-  
-         
+  const exibirPokemons = state.pokedex & state.pokedex.map((pokemon) => {
+    console.log(state.pokedex)
     return (
-      <div>
-           <h1>Meus Pokemóns</h1>
-           {exibirPokemons}
-        <div key={pokemon.name}>
-<div> 
-  <p>#{pokemon.order}</p>
-<div>
-<p>{pokemon.types[0].type.name}</p>
-</div>
-<div>
-<img alt ={pokemon} src={pokemon.sprites.front_default}/>
-</div>
-<button onClick={detalhes}>Detalhes</button>
-<button onClick={removerPokemon}>Excluir</button>
-        <button onClick={home}> Home </button>
+      <div key={pokemon.name}>
+        <div>
+          <p>#{pokemon.order}</p>
+          <div>
+            <p>{pokemon.types[0].type.name}</p>
+          </div>
+          <div>
+            <img alt={pokemon} src={pokemon.sprites.front_default} />
+          </div>
+          <button onClick={detalhes}>Detalhes</button>
+          <button onClick={removerPokemon}>Excluir</button>
+          <button onClick={home}> Home </button>
 
-</div>  
+        </div>
       </div>
-      </div>
-    )}) 
-  } 
+    )
+  })
+
+
+  return (
+    <div>
+      <h1>Meus Pokemóns</h1>
+{exibirPokemons}
+    </div>
+  )
+} 

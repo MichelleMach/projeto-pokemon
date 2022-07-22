de3d3607deec44e158c369cdcabbf025eff30ec2
Laís Rodrigues Martins <navarandadalis@gmail.com>
2022-07-22 17:24:39 -0300

    debus

--- projeto-pokedex/src/page/lista-pokemons/ListaPokemons.js
+++ projeto-pokedex/src/page/lista-pokemons/ListaPokemons.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import Menu from '../../components/menu/Menu'
+import Menu from '../../Components/menu/Menu'
 import * as P from './styled'
 import axios from 'axios'
 import { Pagination } from '@mui/material'
@@ -27,15 +27,15 @@ export default function Home() {
   const conta = 22 * (pgn - 1)
 
 
-  useEffect(() => {
-    axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=${conta}`)
-      .then((res) => {
-        setListaPokemon(res.data.results)
-        console.log(res.data.results)
-      }).catch((err) => {
-        console.log(err.response)
-      })
-  }, [pgn])
+  // useEffect(() => {
+  //   axios.get(`https://pokeapi.co/api/v2/pokemon?limit=21&offset=${conta}`)
+  //     .then((res) => {
+  //       setListaPokemon(res.data.results)
+  //       console.log(res.data.results)
+  //     }).catch((err) => {
+  //       console.log(err.response)
+  //     })
+  // }, [pgn])
 
  
 
